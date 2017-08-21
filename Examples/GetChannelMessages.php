59337492a98ea0f9ce5da247d8c58f0eaa413204
WeCan Group <mutalebe@live.com>
2017-08-22 02:54:01 +0430

    نمونه مثالها
    اصلاح GetChannelMessages.php

--- Examples/GetChannelMessages.php
+++ Examples/GetChannelMessages.php
@@ -1,13 +1,12 @@
 <?php
-// این فایل به این شکل استفاده شود:
-// http://mydomain.ir/mymadeline/GetChannelMessages.php?phone=+989357973301&channel=@wecangp&id=[78,79,80]
-include_once('UserLogin.php'); // خواندن سشن
-
-header('Content-type: application/json'); // اعلام خروجی جی سون به مرورگر
-
-if(isset($_GET['channel']) && isset($_GET['id']))
-  $messages_Messages = $MadelineProto->channels->getMessages(['channel' => $_GET['channel'], 'id' => $_GET['id'] ]);
-  echo json_encode($messages_Messages);
-}else{
-  echo json_encode(["ok"=>false,"des"=>"پارمترهای channel  و id را وارد نمایید."]);
-}
+	// این فایل به این شکل استفاده شود:
+	// http://mydomain.ir/mymadeline/GetChannelMessages.php?phone=+989357973301&channel=@wecangp&ids=78,79,80
+	include_once('UserLogin.php'); // خواندن سشن
+	if(isset($_GET['channel']) && isset($_GET['id'])){
+		$ids = explode(',',$_GET['ids']);
+		$channel = $_GET['channel'];
+		$messages_Messages = $MadelineProto->channels->getMessages(['channel' => $channel, 'id' => $ids  ]);
+		print_r($messages_Messages);
+	}else{
+		echo "پارمترهای channel  و id را وارد نمایید.<br>";
+	}
